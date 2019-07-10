64783761640a86ba8b7671e22dc33f0c1a1619af
Elizabeth Dunphy <edunphy@1drop.de>
2019-07-10 13:44:39 +0200

    [TASK][PRS-259] add javascript for invisible recaptcha

--- Resources/Public/JavaScript/captcha.js
+++ Resources/Public/JavaScript/captcha.js
@@ -1,3 +1,5 @@
+/* global grecaptcha */
+
 !function(d,w,m){
     w.hasReCaptchaAPI||(w.hasReCaptchaAPI=true,m=d.createElement('script'),m.async=1,m.defer=1,m.src='https://www.google.com/recaptcha/api.js',d.body.appendChild(m));
     var g=d.querySelectorAll(".g-recaptcha")[0],a=g.closest("form");
@@ -11,26 +13,109 @@
 
 }(document,window);
 
-function recaptchaCallback(token) {
-    let captchas = document.querySelectorAll(".g-recaptcha");
-    for(let i=0; i<captchas.length; i++) {
-        let inputId = captchas[i].getAttribute('data-form-elementid');
-        document.getElementById(inputId).value=token;
-    }
+/*
+ * helpers
+ */
+
+function getCaptchas() {
+    return document.querySelectorAll('.g-recaptcha');
 }
 
+function getSubmitButtons() {
+    return document.querySelectorAll('form button[type="submit"]');
+}
+
+function setValuesForRecaptchas(token) {
+    getCaptchas().forEach((captcha) => {
+        let inputId = captcha.getAttribute('data-form-elementid');
+        document.getElementById(inputId).value=token;
+    });
+}
+
+/*
+ * code for regular recaptchas
+ */
+
+/**
+ * callback for recaptcha
+ *
+ * @param token
+ */
+function recaptchaCallback(token) {
+    setValuesForRecaptchas(token);
+}
+
+/**
+ * rebuild all captchas
+ */
 function rebuildCaptcha() {
-    let captchas = document.querySelectorAll(".g-recaptcha");
-    for(let i=0; i<captchas.length; i++) {
+    getCaptchas().forEach((captcha) => {
         try {
-            grecaptcha.render(captchas[i]);
+            grecaptcha.render(captcha);
         } catch (e) {
             // do nothing
         }
+    });
+}
+
+/*
+ * code for invisible recaptchas
+ */
+
+let activeButton = null;
+
+function submitForm() {
+    let formId = activeButton.name.match('((\\w|\\d)+-)+(\\w|\\d)+')[0];
+    let form = document.querySelector('form#' + formId);
+    let hiddenField = document.createElement('input');
+    hiddenField.name = activeButton.name;
+    hiddenField.setAttribute('value', activeButton.value);
+    hiddenField.hidden = true;
+    form.appendChild(hiddenField);
+    form.submit();
+}
+
+/**
+ * add token to inputs and submit form
+ *
+ * @param token
+ */
+function invisibleCallback(token) {
+    if(activeButton) {
+        setValuesForRecaptchas(token);
+        submitForm();
+        activeButton = null;
     }
 }
 
-window.recaptchaCallback = recaptchaCallback;
+/**
+ * check if recaptchas are invisible, if so add listener
+ * to stop form submission and validate recaptcha
+ */
+function initSubmitButtons() {
+    if (getCaptchas()[0].getAttribute('data-size') === 'invisible') {
+        getSubmitButtons().forEach((button) => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                activeButton = button;
+                grecaptcha.execute();
+                return false;
+            });
+        });
+    }
+}
+
+/*
+ * initialization
+ */
+
+if (getCaptchas()) {
+    window.recaptchaCallback = recaptchaCallback;
+    window.invisibleCallback = invisibleCallback;
+
+    initSubmitButtons();
+}
 
 if (!Element.prototype.matches) {
     Element.prototype.matches = Element.prototype.msMatchesSelector ||
